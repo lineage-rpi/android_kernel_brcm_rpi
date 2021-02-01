0b75ea796c8f0241806d57ce2a9936c3031971a9
Greg Kroah-Hartman <gregkh@google.com>
2021-02-01 12:51:12 +0100

    Revert "arm64: mm: use single quantity to represent the PA to VA translation"
    This reverts commit 0edc78af73d054d01dc14d7344024362811d8bd3 which is
    commit 7bc1a0f9e1765830e945669c99c59c35cf9bca82 upstream.
    
    It breaks the "supported' ABI for arm64 in that it removes two symbols
    that ended up getting included everywhere:
    	vmemmap
    	physvirt_offset
    so for now, just revert the thing.
    
    Bug: 161946584
    Cc: Ard Biesheuvel <ardb@google.com>
    Cc: Will Deacon <willdeacon@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I213578cd4beff10c1556eb875c1096f6dd4a90ee

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -355,6 +355,20 @@ void __init arm64_memblock_init(void)
 	memstart_addr = round_down(memblock_start_of_DRAM(),
 				   ARM64_MEMSTART_ALIGN);
 
+	physvirt_offset = PHYS_OFFSET - PAGE_OFFSET;
+
+	vmemmap = ((struct page *)VMEMMAP_START - (memstart_addr >> PAGE_SHIFT));
+
+	/*
+	 * If we are running with a 52-bit kernel VA config on a system that
+	 * does not support it, we have to offset our vmemmap and physvirt_offset
+	 * s.t. we avoid the 52-bit portion of the direct linear map
+	 */
+	if (IS_ENABLED(CONFIG_ARM64_VA_BITS_52) && (vabits_actual != 52)) {
+		vmemmap += (_PAGE_OFFSET(48) - _PAGE_OFFSET(52)) >> PAGE_SHIFT;
+		physvirt_offset = PHYS_OFFSET - _PAGE_OFFSET(48);
+	}
+
 	/*
 	 * Remove the memory that we will not be able to cover with the
 	 * linear mapping. Take care not to clip the kernel which may be
@@ -368,16 +382,6 @@ void __init arm64_memblock_init(void)
 					 ARM64_MEMSTART_ALIGN);
 		memblock_remove(0, memstart_addr);
 	}
-
-	/*
-	 * If we are running with a 52-bit kernel VA config on a system that
-	 * does not support it, we have to place the available physical
-	 * memory in the 48-bit addressable part of the linear region, i.e.,
-	 * we have to move it upward. Since memstart_addr represents the
-	 * physical address of PAGE_OFFSET, we have to *subtract* from it.
-	 */
-	if (IS_ENABLED(CONFIG_ARM64_VA_BITS_52) && (vabits_actual != 52))
-		memstart_addr -= _PAGE_OFFSET(48) - _PAGE_OFFSET(52);
 
 	/*
 	 * Apply the memory limit if it was set. Since the kernel may be loaded
